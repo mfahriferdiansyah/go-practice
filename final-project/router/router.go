3d7cda0770df8c31a4bb4852c785e3b64ad40f3a
mfahriferdiansyah <xkadzuvic@gmail.com>
2024-05-20 00:15:08 +0700

    refactor: trying to fix railway drouting only read Get request

--- final-project/router/router.go
+++ final-project/router/router.go
@@ -10,29 +10,30 @@ import (
 func RouterSetup() *gin.Engine {
 	router := gin.Default()
 
-	userRouter := router.Group("/auth")
-	{
-		userRouter.POST("/register", controllers.Register)
-		userRouter.POST("/login", controllers.Login)
-	}
+	router.Use(middleware.CORSMiddleware())
+	// userRouter := router.Group("/auth")
+	// {
+	router.POST("/auth/register", controllers.Register)
+	router.POST("/auth/login", controllers.Login)
+	// }
 
-	productRouter := router.Group("/products")
-	{
-		productRouter.GET("/variants/:variantUUID", controllers.GetVariantByUUID)
-		productRouter.GET("/variants/", controllers.GetVariants)
+	// productRouter := router.Group("/products")
+	// {
+	router.GET("/products/variants/:variantUUID", controllers.GetVariantByUUID)
+	router.GET("/products/variants/", controllers.GetVariants)
 
-		productRouter.POST("/variants/", middleware.Authentication(), middleware.CreateVariantAuthorization(), controllers.CreateVariant)
-		productRouter.PUT("/variants/:variantUUID", middleware.Authentication(), middleware.VariantAuthorization(), controllers.UpdateVariant)
-		productRouter.DELETE("/variants/:variantUUID", middleware.Authentication(), middleware.VariantAuthorization(), controllers.DeleteVariant)
+	router.POST("/products/variants/", middleware.Authentication(), middleware.CreateVariantAuthorization(), controllers.CreateVariant)
+	router.PUT("/products/variants/:variantUUID", middleware.Authentication(), middleware.VariantAuthorization(), controllers.UpdateVariant)
+	router.DELETE("/products/variants/:variantUUID", middleware.Authentication(), middleware.VariantAuthorization(), controllers.DeleteVariant)
 
-		productRouter.GET("/:productUUID", controllers.GetProductByUUID)
-		productRouter.GET("/", controllers.GetProducts)
+	router.GET("/products/:productUUID", controllers.GetProductByUUID)
+	router.GET("/products/", controllers.GetProducts)
 
-		productRouter.Use(middleware.Authentication())
-		productRouter.POST("/", controllers.CreateProduct)
-		productRouter.PUT("/:productUUID", middleware.ProductAuthorization(), controllers.UpdateProduct)
-		productRouter.DELETE("/:productUUID", middleware.ProductAuthorization(), controllers.DeleteProduct)
-	}
+	// router.Use(middleware.Authentication())
+	router.POST("/products/", middleware.Authentication(), controllers.CreateProduct)
+	router.PUT("/products/:productUUID", middleware.Authentication(), middleware.ProductAuthorization(), controllers.UpdateProduct)
+	router.DELETE("/products/:productUUID", middleware.Authentication(), middleware.ProductAuthorization(), controllers.DeleteProduct)
+	// }
 
 	return router
 }
